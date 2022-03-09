34d9f03f478ab68f4f2a9b570f0bb8ddf3e89b16
rodrigo <rodrigo.fantibon@hotmail.com>
2022-03-09 18:12:33 -0300

    configuramos o .env

--- aula5.js
+++ aula5.js
@@ -1,1 +1,11 @@
-api.openweathermap.org/data/2.5/forecast?q=SÃO%20PAULO&appid=24d25bbf2d4928f07700e16e22663cb4
+require('dotenv').config()
+
+const axios = require ('axios')
+console.log(process.env.APP_KEY)
+
+
+//---------------------------------------------------
+//nao tem haver com a parte de cima
+const {v1, v2,v3} = process.env //desistruturação buscando de outra classe
+console.log (v1,v2,v3)          //buscando do arquivo .env
+//---------------------------------------------
